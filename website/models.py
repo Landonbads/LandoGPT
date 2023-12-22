1ff39301bf335fba70df3ca9afc67bd2330807f7
Lands142 <lands145@gmail.com>
2023-12-22 17:04:58 -0500

    added User class for database

--- website/models.py
+++ website/models.py
@@ -2,3 +2,10 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100),unique=True)
+    password = db.Column(db.String(100))
+
+# tbd MessageHistory object
