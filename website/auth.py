b8301abce0b02ad05157072c98ba0a967aba43d0
Landon <lands145@gmail.com>
2023-12-23 21:14:17 -0500

    finalized registration with hashed pw

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,8 @@
 from flask import Blueprint, render_template,request,flash, redirect, url_for
 from .models import User
 from .models import db
+from sqlalchemy.exc import IntegrityError
+import bcrypt
 
 # create blueprint for login/signup authentication
 auth = Blueprint("auth",__name__)
@@ -15,6 +17,8 @@ def sign_up():
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
+        # hash password before storing in database
+        hashed_password = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
         
         # flash an error if email or password aren't formatted correctly
         if len(email) < 7:
@@ -24,24 +28,30 @@ def sign_up():
         elif len(username) < 2:
             flash('First name should be at least 1 character!','danger')
         else:
-            # create user with username,email, and password given via request
-            user = User(
-                username=username,
-                email=email,
-                password=password
-            )   
-            # add user to database
-            db.session.add(user)
-            # commit changes
-            db.session.commit()
-            return redirect(url_for('mel'))
-    # if not registering user, just reload page
-    else:
-        return render_template("signup.html")
+            # try catch to handle integrity error (if email already exists in database)
+            try:
+                # create user with username,email, and hashed password given via request
+                user = User(
+                    username=username,
+                    email=email,
+                    password=hashed_password
+                )  
+                
+                # add user to database
+                db.session.add(user)
+                # commit changes
+                db.session.commit()
+                # flash a message to notify user of successful account creation
+                flash('Account successfully created!','success')
+            except IntegrityError:
+                flash('Email already signed up!', 'danger')
+
+
+    return render_template("signup.html")
 
 @auth.route("/users",)
 def user_list():
-    users = db.session.execute(db.select(User.email)).scalars().all()
+    users = db.session.execute(db.select(User.password)).scalars().all()
     return render_template("home.html",emails=users)
 
 
