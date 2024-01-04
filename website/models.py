e3b2e8dd1442660aacbf9a1378227eb8cc9930fd
Landon <lands145@gmail.com>
2024-01-04 16:24:56 -0500

    sqlite to postgresql conversion continues

--- website/models.py
+++ website/models.py
@@ -3,9 +3,9 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    email = db.Column(db.String(250),unique=True)
-    password = db.Column(db.String(255))
+    username = db.Column(db.String(1000))
+    email = db.Column(db.String(1000),unique=True)
+    password = db.Column(db.String(1000))
     credits = db.relationship('Credits')
 
 class Credits(db.Model):
