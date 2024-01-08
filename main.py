de577e29cd10095a0b7587deb81978107423575a
Landon <lands145@gmail.com>
2024-01-07 20:35:58 -0500

    TEST

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from website import create_database
 
 app = create_app()
 create_database(app)
+
 # limits running to only this file
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     
